e50bad870ab83c5c34119e6013881e65200c635d
Bima Afrizal Malna <bimaafrizalmalna@student.uns.ac.id>
2025-05-27 14:18:16 +0700

    init

---
+++ app.py
@@ -0,0 +1,15 @@
+# print("Halo, ini program Python di Docker!")
+
+# for i in range(5):
+#     print(f"Baris ke-{i+1} dari Docker container")
+
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Halo dari Flask di Docker!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
